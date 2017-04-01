1e68968c0f83a6983064a973ed6dd2ccb7001894
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2017-04-01 12:30:40 +0200

    Include make file for gtest code in repository

---
+++ Dynamic_HD_Cpp_Code/src/gtest/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../src/gtest/gtest-death-test.cc \
+../src/gtest/gtest-filepath.cc \
+../src/gtest/gtest-port.cc \
+../src/gtest/gtest-printers.cc \
+../src/gtest/gtest-test-part.cc \
+../src/gtest/gtest-typed-test.cc \
+../src/gtest/gtest.cc 
+
+CC_DEPS += \
+./src/gtest/gtest-death-test.d \
+./src/gtest/gtest-filepath.d \
+./src/gtest/gtest-port.d \
+./src/gtest/gtest-printers.d \
+./src/gtest/gtest-test-part.d \
+./src/gtest/gtest-typed-test.d \
+./src/gtest/gtest.d 
+
+OBJS += \
+./src/gtest/gtest-death-test.o \
+./src/gtest/gtest-filepath.o \
+./src/gtest/gtest-port.o \
+./src/gtest/gtest-printers.o \
+./src/gtest/gtest-test-part.o \
+./src/gtest/gtest-typed-test.o \
+./src/gtest/gtest.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/gtest/%.o: ../src/gtest/%.cc
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	$(GPLUSPLUS) -I"../include" -I"../." -I"../src" -O3 -Wall -c -fmessage-length=0 -std=gnu++11 $(STDLIB_OPT) -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
