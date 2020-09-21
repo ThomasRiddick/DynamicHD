0867054aafc19516616c4bb83ab386038516e186
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2020-09-21 10:53:38 +0200

    Increment version number

--- Dynamic_HD_bash_scripts/hd_operator_driver.sh
+++ Dynamic_HD_bash_scripts/hd_operator_driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Running Version 3.7 of the HD Command Line Operator Code"
+echo "Running Version 3.8 of the HD Command Line Operator Code"
 
 #Define module loading function
 function load_module
