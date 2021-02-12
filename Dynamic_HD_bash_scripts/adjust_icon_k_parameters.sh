e757304215455ea102a084709b252cfd76833770
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2021-02-12 21:13:03 +0100

    Add script to adjust k parameters of an icon hdpara file

---
+++ Dynamic_HD_bash_scripts/adjust_icon_k_parameters.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+icon_para_input_filename=${1}
+icon_para_output_filename=${2}
+resolution=${3}
+
+if [[ ${resolution} == 'r2b8' ]]; then
+  new_arf_k=0.027
+  arf_k_adjustment_thres=0.1
+  new_alf_k=4.82
+  alf_k_adjustment_thres=8.0
+else
+  exit 1
+fi
+
+ncap2 -s "ALF_K=(float(ALF_K<=${alf_k_adjustment_thres})*ALF_K)+(float(ALF_K>${alf_k_adjustment_thres})*${new_alf_k});ARF_K=(float(ARF_K<=${arf_k_adjustment_thres})*ARF_K)+(float(ARF_K>${arf_k_adjustment_thres})*${new_arf_k});" ${icon_para_input_filename} ${icon_para_output_filename}_temp
+cdo replace ${icon_para_input_filename} ${icon_para_output_filename}_temp ${icon_para_output_filename}
+rm ${icon_para_output_filename}_temp
