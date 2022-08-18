2d2cfa557aecdd9626d2923c4e91e9de84cf3cc2
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2022-08-18 13:24:46 +0200

    Add no_env_gen to dynamic lake top level driver

--- Dynamic_HD_bash_scripts/dynamic_lake_top_level_driver.sh
+++ Dynamic_HD_bash_scripts/dynamic_lake_top_level_driver.sh
@@ -231,6 +231,21 @@ else
 	echo "Format of no_modules flag (${no_modules}) is unknown, please use True/False or T/F" 1>&2
 	exit 1
 fi
+
+no_env_gen=${no_env_gen:-"false"}
+if [[ $no_env_gen == "true" ]] || [[ $no_env_gen == "t" ]]; then
+        no_env_gen=true
+elif [[ $no_env_gen == "false" ]] || [[ $no_env_gen == "f" ]]; then
+        no_env_gen=false
+else
+        echo "Format of no_env_gen flag (${no_env_gen}) is unknown, please use True/False or T/F" 1>&2
+        exit 1
+fi
+
+if ${no_conda} ; then
+no_env_gen=false
+fi
+
 shopt -u nocasematch
 
 #Change to the working directory
@@ -289,13 +304,15 @@ if ! $no_modules && ! $no_conda ; then
 	fi
 fi
 
-if ! $no_conda ; then
+if ! $no_conda && ! $no_env_gen ; then
 	if $compilation_required && conda info -e | grep -q "dyhdenv3"; then
 		conda env remove --yes --name dyhdenv3
 	fi
 	if ! conda info -e | grep -q "dyhdenv3"; then
 		${source_directory}/Dynamic_HD_bash_scripts/regenerate_conda_environment.sh $no_modules
 	fi
+fi
+if ! $no_conda ; then
 	source activate dyhdenv3
 fi
 
