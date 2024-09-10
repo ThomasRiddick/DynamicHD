42b57fb77d96d047ffc2e7f26b45dfff650f85bd
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2024-09-10 15:05:05 +0200

    Pin python version to 3.11 when generating mamba env
    Python 3.12 doesn't include distutils which causes a failure when using meson

--- Dynamic_HD_bash_scripts/regenerate_conda_environment.sh
+++ Dynamic_HD_bash_scripts/regenerate_conda_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mamba env remove -n dyhdenv_mamba
-mamba create --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch -n dyhdenv_mamba
+mamba create --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch -n dyhdenv_mamba python=3.11
 source activate dyhdenv_mamba
 mamba install --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch cdo netcdf4 numpy matplotlib xarray cython scipy python-cdo mpi4py jinja2 meson
 mamba list -e
