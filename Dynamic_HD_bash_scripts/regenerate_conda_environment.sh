99ca11e06967999b24a234b9fd44ac662f44666e
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2024-09-10 13:23:24 +0200

    Alter conda environment generation script to use mamba and conda-forge only

--- Dynamic_HD_bash_scripts/regenerate_conda_environment.sh
+++ Dynamic_HD_bash_scripts/regenerate_conda_environment.sh
@@ -1,53 +1,8 @@
 #!/bin/bash
 
-no_modules=${1}
-
-#Define module loading function
-function load_module
-{
-module_name=$1
-if [[ $(hostname -d) == "lvt.dkrz.de" ]]; then
-  module load ${module_name}
-else
-  eval "eval `/usr/bin/tclsh /sw/share/Modules-4.2.1/libexec/modulecmd.tcl bash load ${module_name}`"
-fi
-}
-
-#Define module unloading function (only works on hpc)
-function unload_module
-{
-module_name=$1
-if echo $LOADEDMODULES | fgrep -q ${module_name} ; then
-  module unload $module_name
-fi
-}
-
-if ! $no_modules ; then
-  if [[ $(hostname -d) == "lvt.dkrz.de" ]]; then
-    source /etc/profile
-    unload_module netcdf_c
-    unload_module imagemagick
-    unload_module cdo/1.7.0-magicsxx-gcc48
-    unload_module python
-  else
-    unload_module netcdf_c
-    load_module anaconda3
-  fi
-fi
-
-if ! $no_modules; then
-  if [[ $(hostname -d) == "lvt.dkrz.de" ]]; then
-    load_module python3
-  else
-    load_module anaconda3
-  fi
-fi
-
-conda_command=mamba
-conda-env remove -n dyhdenv3
-$conda_command config --add channels conda-forge
-$conda_comannd create --yes -n dyhdenv3
-source activate dyhdenv3
-$conda_command install --yes cdo=1.9.9 netcdf4 numpy matplotlib xarray cython scipy python-cdo configparser mpi4py
-$conda_command list -e
+mamba env remove -n dyhdenv_mamba
+mamba create --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch -n dyhdenv_mamba
+source activate dyhdenv_mamba
+mamba install --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch cdo netcdf4 numpy matplotlib xarray cython scipy python-cdo mpi4py jinja2 meson
+mamba list -e
 source deactivate
