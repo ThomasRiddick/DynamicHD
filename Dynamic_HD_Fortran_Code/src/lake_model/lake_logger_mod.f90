58b2e8103d0b762bff81ab68b574f1d2b8143964
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2025-09-04 16:44:56 +0200

    Improvements to lake logger in fortran for new lake scheme

--- Dynamic_HD_Fortran_Code/src/lake_model/lake_logger_mod.f90
+++ Dynamic_HD_Fortran_Code/src/lake_model/lake_logger_mod.f90
@@ -14,7 +14,7 @@ integer, parameter :: info_dump_timestep = 99999
 
 type lake_logger
   integer :: timestep
-  integer, dimension(print_interval,4) :: lake_process_log
+  integer, dimension(print_interval,3) :: lake_process_log
   real(dp) :: lake_volumes(print_interval)
   character(len=30), dimension(print_interval) :: process_names
   integer :: log_position
@@ -70,31 +70,35 @@ subroutine increment_timestep(this)
     end if
 end subroutine increment_timestep
 
-subroutine create_info_dump(this,water_to_lakes,lake_volumes, &
-                            nlat,nlon,nlat_coarse,nlon_coarse)
-  class(lake_logger) :: this
-  real(dp), dimension(:,:),pointer :: water_to_lakes
-  real(dp), dimension(:,:),pointer :: lake_volumes
-  integer ::  nlat,nlon,nlat_coarse,nlon_coarse
+subroutine create_info_dump(this,water_to_lakes,lake_volumes,_NPOINTS_HD_)
+  class(lake_logger), intent(in) :: this
+  real(dp), dimension(_DIMS_), pointer, intent(in) :: water_to_lakes
+  real(dp), dimension(:), pointer, intent(in) :: lake_volumes
+  _DEF_NPOINTS_HD_ _INTENT_in_
   character(len = *),parameter :: info_dump_filename = &
     "/Users/thomasriddick/Documents/data/temp/info_dump.nc"
   integer :: ncid,varid_volumes,varid_water_to_lakes
-  integer :: lat_dimid,lon_dimid,lat_dimid_coarse,lon_dimid_coarse
-  integer, dimension(2) :: dimids,dimids_coarse
+  _IF_USE_SINGLE_INDEX_
+  ! Not implemented
+  _ELSE_
+  integer :: lat_dimid_hd,lon_dimid_hd
+  _END_IF_USE_SINGLE_INDEX_
+  integer, dimension(2) :: dimids_hd
     if(this%timestep == info_dump_timestep) then
         call check_return_code(nf90_create(info_dump_filename,nf90_noclobber,ncid))
-        call check_return_code(nf90_def_dim(ncid, "lat",nlat,lat_dimid))
-        call check_return_code(nf90_def_dim(ncid, "lon",nlon,lon_dimid))
-        call check_return_code(nf90_def_dim(ncid, "lat_coarse",nlat_coarse,&
-                                            lat_dimid_coarse))
-        call check_return_code(nf90_def_dim(ncid, "lon_coarse",nlon_coarse,&
-                                            lon_dimid_coarse))
-        dimids = (/lat_dimid,lon_dimid/)
-        dimids_coarse = (/lat_dimid_coarse,lon_dimid_coarse/)
+        _IF_USE_SINGLE_INDEX_
+        ! Not implemented
+        _ELSE_
+        call check_return_code(nf90_def_dim(ncid, "lat",nlat_hd,&
+                                            lat_dimid_hd))
+        call check_return_code(nf90_def_dim(ncid, "lon",nlon_hd,&
+                                            lon_dimid_hd))
+        dimids_hd = (/lat_dimid_hd,lon_dimid_hd/)
+        _END_IF_USE_SINGLE_INDEX_
         call check_return_code(nf90_def_var(ncid, "lake_volumes",&
                                             nf90_real, dimids,varid_volumes))
         call check_return_code(nf90_def_var(ncid, "water_to_lakes",&
-                                            nf90_real, dimids_coarse,&
+                                            nf90_real, hd,&
                                             varid_water_to_lakes))
         call check_return_code(nf90_enddef(ncid))
         call check_return_code(nf90_put_var(ncid,varid_volumes,lake_volumes))
@@ -105,24 +109,22 @@ subroutine create_info_dump(this,water_to_lakes,lake_volumes, &
     deallocate(lake_volumes)
 end subroutine create_info_dump
 
-subroutine log_process(this,lake_number,lake_center_lat,lake_center_lon, &
+subroutine log_process(this,lake_number,active_lake, &
                        lake_type,lake_volume,process_name)
-  class(lake_logger) :: this
-  integer :: lake_number
-  integer :: lake_center_lat
-  integer :: lake_center_lon
-  integer :: lake_type
-  real(dp) :: lake_volume
-  character(len=*) :: process_name
+  class(lake_logger), intent(inout) :: this
+  integer, intent(in) :: lake_number
+  logical, intent(in) :: active_lake
+  integer, intent(in) :: lake_type
+  real(dp), intent(in) :: lake_volume
+  character(len=*), intent(in) :: process_name
   character(len=30) :: process_name_fixed_length
     write(process_name_fixed_length,*) process_name
     if(this%logging_status) then
       this%log_position = this%log_position + 1
       this%timestep_total_output = this%timestep_total_output + 1
       this%lake_process_log(this%log_position,1) = lake_number
-      this%lake_process_log(this%log_position,2) = lake_center_lat
-      this%lake_process_log(this%log_position,3) = lake_center_lon
-      this%lake_process_log(this%log_position,4) = lake_type
+      this%lake_process_log(this%log_position,2) = int(active_lake)
+      this%lake_process_log(this%log_position,3) = lake_type
       this%lake_volumes(this%log_position) = lake_volume
       this%process_names(this%log_position) = process_name
       if (this%timestep_total_output >= maximum_output_per_timestep) then
@@ -148,9 +150,8 @@ subroutine print_log(this)
   integer :: i
     if (this%log_position > 0) then
       do i = 1,this%log_position
-        write(*,'(4(X,I8),3X,ES14.7,3X,A)') this%lake_process_log(i,1),this%lake_process_log(i,2),&
-                   this%lake_process_log(i,3),this%lake_process_log(i,4),&
-                   this%lake_volumes(i),this%process_names(i)
+        write(*,'(3(X,I8),3X,ES14.7,3X,A)') this%lake_process_log(i,1),this%lake_process_log(i,2),&
+                   this%lake_process_log(i,3),this%lake_volumes(i),this%process_names(i)
       end do
     end if
 end subroutine print_log
@@ -169,26 +170,25 @@ subroutine increment_timestep_wrapper()
     call global_lake_logger%increment_timestep()
 end subroutine increment_timestep_wrapper
 
-subroutine log_process_wrapper(lake_number,lake_center_lat,lake_center_lon,&
+subroutine log_process_wrapper(lake_number,active_lake,&
                                lake_type,lake_volume,process_name)
-  integer :: lake_number
-  integer :: lake_center_lat
-  integer :: lake_center_lon
-  integer :: lake_type
-  real(dp) :: lake_volume
-  character(len=*) :: process_name
-    call global_lake_logger%log_process(lake_number,lake_center_lat,&
-                                        lake_center_lon,lake_type,&
-                                        lake_volume,process_name)
+  integer, intent(in) :: lake_number
+  logical, intent(in) :: active_lake
+  integer, intent(in) :: lake_type
+  real(dp), intent(in) :: lake_volume
+  character(len=*), intent(in) :: process_name
+    call global_lake_logger%log_process(lake_number,active_lake, &
+                                        lake_type,lake_volume,process_name)
 end subroutine log_process_wrapper
 
 subroutine create_info_dump_wrapper(water_to_lakes,lake_volumes, &
-                                    nlat,nlon,nlat_coarse,nlon_coarse)
-  real(dp), dimension(:,:),pointer :: water_to_lakes
-  real(dp), dimension(:,:),pointer :: lake_volumes
-  integer ::  nlat,nlon,nlat_coarse,nlon_coarse
+                                    _NPOINTS_LAKE_,_NPOINTS_HD_)
+  real(dp), dimension(_DIMS_),pointer :: water_to_lakes
+  real(dp), dimension(:),pointer :: lake_volumes
+  _DEF_NPOINTS_LAKE_ _INTENT_in_
+  _DEF_NPOINTS_HD_ _INTENT_in_
     call global_lake_logger%create_info_dump(water_to_lakes,lake_volumes, &
-                                             nlat,nlon,nlat_coarse,nlon_coarse)
+                                             _NPOINTS_LAKE_,_NPOINTS_HD_)
 end subroutine create_info_dump_wrapper
 
 function is_logging()
