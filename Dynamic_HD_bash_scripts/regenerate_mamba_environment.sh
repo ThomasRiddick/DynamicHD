f16c27cf7b73e29826d180b92d31c2383a38eef7
Thomas Riddick <thomasriddick@mpimet.mpg.de>
2025-12-16 14:51:52 +0100

    Enable basic plotter in icon hdpara generation script and adjust mamba environment

--- Dynamic_HD_bash_scripts/regenerate_mamba_environment.sh
+++ Dynamic_HD_bash_scripts/regenerate_mamba_environment.sh
@@ -3,6 +3,6 @@
 mamba env remove -n dyhdenv_mamba
 mamba create --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch -n dyhdenv_mamba python=3.11
 source activate dyhdenv_mamba
-mamba install --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch cdo netcdf4 numpy matplotlib xarray cython scipy python-cdo mpi4py jinja2 meson
+mamba install --yes --override-channels -c conda-forge/linux-64 -c conda-forge/noarch cdo netcdf4 numpy matplotlib xarray cython scipy python-cdo mpi4py jinja2 meson matplotlib cartopy
 mamba list -e
 source deactivate
